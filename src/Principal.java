52918248f554fee6fe45a5093a0cbb4a7f24b0ac
lenovo <lenovo@192.168.1.9>
2021-06-06 15:20:50 -0600

    Formulario de producto

---
+++ src/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Factura v1 = new Factura();
+        v1.setVisible(true);
+                
+    }
+}
